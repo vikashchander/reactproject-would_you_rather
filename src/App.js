4642399a8a8d616baa59d8fb9914546c013d4f54
vikash chander <vikashchander78384@gmail.com>
2020-04-06 17:50:06 +0530

    call api and create basic structure with redux

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      {/* <NavBar />
+      <MainContent />
+      <Footer /> */}
+    </Fragment>
+  );
+}
+
+export default App;
